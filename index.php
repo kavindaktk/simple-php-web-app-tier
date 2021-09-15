4769fe2039741ad47d9bb73fa1e3a7d4533dac2a
kavindaktk <36921119+kavindaktk@users.noreply.github.com>
2021-09-15 18:27:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?PHP 
-echo "This is simple web app";
+<html>
+<head>
+	<title>Data Submission Form</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
 
-?>
+    <h2>Inputs</h2>
+
+    <form action="registration_form.php" method="POST"> 
+      My Phrase:
+      <input type="text" name="MyPhrase"> 
+ 
+        <input type="hidden" name="form_submitted" value="1" />
+
+        <input type="submit" value="Submit">
+
+    </form>
+</body>
+</html>
