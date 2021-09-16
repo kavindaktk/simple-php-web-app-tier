b37bc869660b9e7344f2301c6a3aee2ec670804c
kavindaktk <36921119+kavindaktk@users.noreply.github.com>
2021-09-17 02:26:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,44 @@
 <head>
 	<title>Data Submission Form</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+<script>
+//var inputVal = document.querySelector('MyPhrase').value;
+var inputVal = document.getElementById("MyPhrase").value;
+function test(inputVal){
+	var inputVal = document.getElementById("MyPhrase").value;
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://54.224.123.176/myapi/public/create_phrase/data");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+		   if (xhr.readyState === 4) {
+			         console.log(xhr.status);
+				       console.log(xhr.responseText);
+				    }};
+		   var data = `{
+		     "myPhrase":"` + inputVal + `"}`;
+
+		   xhr.send(data);
+
+}
+
+</script>
+
+
 </head>
+
 <body>
 
     <h2>Inputs</h2>
 
-    <form action="registration_form.php" method="POST"> 
       My Phrase:
-      <input type="text" name="MyPhrase"> 
+      <input type="text" id="MyPhrase"  name="MyPhrase"> 
  
-        <input type="hidden" name="form_submitted" value="1" />
+	<button onclick="test()">Save Data</button>
 
-        <input type="submit" value="Submit">
 
-    </form>
 </body>
 </html>
+
